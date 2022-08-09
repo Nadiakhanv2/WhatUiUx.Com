f0a085fb14c95f024815c15e82a543bc9972eacb
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-09 18:38:08 +0500

    .

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>
-      header
+    <div className="gpt3Header sectionPadd">
+      <div className="gpt3HeaderContent">
+        <h1 className="gradientTxt">
+          Let's Build Something Amazing With GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed only for travelling assisstance indulgence .Not thought all
+          exercise blessing. Indulgence was everything joy alternation
+          boisterous the attachment. Party we years to order allow asked of.
+        </p>
+        <div className="gpt3HeaderContentInput">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3HeaderContentPeople">
+          <Image />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
