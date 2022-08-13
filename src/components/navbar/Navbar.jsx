eb37280379ac266d07920cad63535d7b4f306af7
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-13 14:37:42 +0500

    useState & togglebtn

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import React , {useState} from "react";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 // BEM --- Block Element Modifier
+const Menu = () => {
+  return (
+  <>
+    <p>
+      <a href="/">Home</a>
+    </p>
+    <p>
+      <a href="/wgpt3">What is GPT?</a>
+    </p>
+    <p>
+      <a href="/possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="/features">Case Studies</a>
+    </p>
+    <p>
+      <a href="/blog">Library</a>
+    </p>
+  </>);
+}
 
 const Navbar = (props) => {
+const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3Navbar">
       <div className="gpt3NavbarLinks">
@@ -16,29 +38,41 @@ const Navbar = (props) => {
           />
         </div>
         <div className="gpt3NavbarLinksConntainer">
-          <p>
-            <a href="/">Home</a>
-          </p>
-          <p>
-            <a href="/wgpt3">What is GPT?</a>
-          </p>
-          <p>
-            <a href="/possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="/features">Case Studies</a>
-          </p>
-          <p>
-            <a href="/blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="gpt3NavbarSign">
         <p>Sign in</p>
-        <button type='button'>Sign up</button>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3NavbarMenu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3NavbarMenuContainer scaleUpCenter">
+            <div className="gpt3NavbarMenuContainerLinks">
+              <Menu />
+              <div className="gpt3NavbarMenuContainerLinksSign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
