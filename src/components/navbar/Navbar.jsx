e2441b68821298fd926db92999b194a61a77c5d7
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-13 14:02:46 +0500

    .

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
-const Navbar = () => {
+// BEM --- Block Element Modifier
+
+const Navbar = (props) => {
   return (
-    <div>
-      navbar
+    <div className="gpt3Navbar">
+      <div className="gpt3NavbarLinks">
+        <div className="gpt3NavbarLinksLogo">
+          <img
+            src={props.logo}
+            alt="GPT3 Logo"
+            width={93}
+            className="img-fluid"
+          />
+        </div>
+        <div className="gpt3NavbarLinksConntainer">
+          <p>
+            <a href="/">Home</a>
+          </p>
+          <p>
+            <a href="/wgpt3">What is GPT?</a>
+          </p>
+          <p>
+            <a href="/possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="/features">Case Studies</a>
+          </p>
+          <p>
+            <a href="/blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3NavbarSign">
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
